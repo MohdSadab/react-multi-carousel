1a566c5d2fefb664fe7965c634fed2d97ad873bf
MohdSadab <31180132+MohdSadab@users.noreply.github.com>
2022-03-16 16:33:41 +0530

    Initial commit

---
+++ components/Simple.js
@@ -0,0 +1,61 @@
+import Carousel from "react-multi-carousel";
+import { Image } from "semantic-ui-react";
+
+const responsive = {
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 3,
+    paritialVisibilityGutter: 60
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 2,
+    paritialVisibilityGutter: 50
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+    paritialVisibilityGutter: 30
+  }
+};
+const images = [
+  "https://d36qrhglnzidxv.cloudfront.net/uploads/2022…20Down%20Facebook,%20Instagram%20in%20Europe.webp",
+  "https://d36qrhglnzidxv.cloudfront.net/uploads/2022…20Down%20Facebook,%20Instagram%20in%20Europe.webp",
+  "https://images.unsplash.com/photo-1550133730-695473e544be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550167164-1b67c2be3973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550338861-b7cfeaf8ffd8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550223640-23097fc71cb2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550353175-a3611868086b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://d36qrhglnzidxv.cloudfront.net/uploads/2022…20Down%20Facebook,%20Instagram%20in%20Europe.webp",
+  "https://images.unsplash.com/photo-1549737328-8b9f3252b927?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1549833284-6a7df91c1f65?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1549985908-597a09ef0a7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
+  "https://images.unsplash.com/photo-1550064824-8f993041ffd3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60"
+];
+
+// Because this is an inframe, so the SSR mode doesn't not do well here.
+// It will work on real devices.
+const Simple = ({ deviceType }) => {
+  return (
+    <Carousel
+      ssr
+      partialVisbile
+      deviceType={deviceType}
+      itemClass="image-item"
+      responsive={responsive}
+    >
+      {console.log("Hello")}
+      {images.slice(0, 5).map((image) => {
+        return (
+          <Image
+            draggable={false}
+            style={{ width: "100%", height: "100%" }}
+            src={image}
+          />
+        );
+      })}
+    </Carousel>
+  );
+};
+
+export default Simple;
